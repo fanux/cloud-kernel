f7f2160eb25779f6ca6f1cf08e6bbf1ef5424673
fanux <fhtjob@hotmail.com>
2019-09-23 12:31:39 +0800

    [CI SKIP]

--- kube/shell/master.sh
+++ kube/shell/master.sh
@@ -3,6 +3,8 @@ mkdir ~/.kube
 cp /etc/kubernetes/admin.conf ~/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f ../conf/calico.yaml
+sleep 5
+kubectl apply -f ../conf/calico.yaml
 
 echo "==================================================================="
 echo "      安装常见问题见此链接评论区：http://sealyun.com/faq"
