1fdebf29a28f5af7d0154c7ef91ac5565616cf6a
steven <fhtjob@hotmail.com>
2020-02-24 18:00:53 +0800

    Update kubelet-pre-start.sh

--- kube/bin/kubelet-pre-start.sh
+++ kube/bin/kubelet-pre-start.sh
@@ -4,7 +4,11 @@ modprobe -- ip_vs
 modprobe -- ip_vs_rr
 modprobe -- ip_vs_wrr
 modprobe -- ip_vs_sh
-modprobe -- nf_conntrack_ipv4
+if [[ $(uname -r |cut -d . -f1) -ge 4 && $(uname -r |cut -d . -f2) -ge 19 ]]; then
+  modprobe -- nf_conntrack
+else
+  modprobe -- nf_conntrack_ipv4
+fi
 
 cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
