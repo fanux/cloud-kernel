f588408d0bfff221dbf050f6198852a01dc63283
cuisongliu <cuisongliu@qq.com>
2020-11-15 02:28:51 +0800

    fix(master): fix package.sh and market script

--- oss.sh
+++ oss.sh
@@ -3,27 +3,3 @@ cd /tmp
 md5=$(md5sum /tmp/kube$1.tar.gz | awk  '{print $1}')
 ossutil64 -c oss-config cp /tmp/kube$1.tar.gz oss://sealyun/$md5-$1/kube$1.tar.gz
 sshcmd --passwd $2 --host store.lameleg.com --cmd "sh release-k8s-new.sh $1 $md5"
-
-cat > k8s.yaml << EOF
-market:
-  body:
-    spec:
-      name: v$1
-      price: 50
-      product:
-        class: cloud_kernel
-        productName: kubernetes
-      url: https://sealyun.oss-cn-beijing.aliyuncs.com/$md5-$1/kube$1.tar.gz
-    status:
-      productVersionStatus: 2
-  kind: productVersion
-EOF
-
-
-
-marketctl create -f k8s.yaml --domain http://market.sealyun.com --token $3
-marketctl update -f k8s.yaml --status --domain http://market.sealyun.com --token $3
-
-curl "https://oapi.dingtalk.com/robot/send?access_token=$4" \
-   -H "Content-Type: application/json" \
-   -d "{\"msgtype\":\"link\",\"link\":{\"text\":\"kubernetes自动发布版本v$1,详细信息请看https://github.com/kubernetes/kubernetes/releases/tag/v$1\",\"title\":\"kubernetes版本发布成功\",\"picUrl\":\"\",\"messageUrl\":\"http://store.lameleg.com\"}}"
--- package.sh
+++ package.sh
@@ -70,7 +70,7 @@ remotecmd "cd /tmp/ && wget http://gosspublic.alicdn.com/ossutil/1.6.19/ossutil6
            mv sshcmd /usr/sbin/sshcmd && \
            wget https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/latest/marketctl && chmod a+x marketctl && \
            mv marketctl /usr/sbin/marketctl"
-remotecmd "cd /root/cloud-kernel/ && sh oss.sh $1 $2 ${MARKET_TOKEN} ${DING_TOKEN}"
+remotecmd "cd /root/cloud-kernel/ && sh oss.sh $1 $2"
 
 #sshcmd --passwd $2 --host store.lameleg.com --cmd "sh release-k8s.sh $1 $FIP"
 
