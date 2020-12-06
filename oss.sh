9d5198e4f125b03fb1019cb3b1abd549836e294b
cuisongliu <cuisongliu@qq.com>
2020-12-06 15:31:37 +0800

    fix(master): fix package.sh and market script

--- oss.sh
+++ oss.sh
@@ -3,7 +3,7 @@ cd /tmp
 md5=$(md5sum /tmp/kube$1.tar.gz | awk  '{print $1}')
 #ossutil64 -c oss-config cp /tmp/kube$1.tar.gz oss://sealyun/$md5-$1/kube$1.tar.gz
 #sshcmd --passwd $2 --host store.lameleg.com --cmd "sh release-k8s-new.sh $1 $md5"
-wget https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v1.0.1/linux_amd64/marketctl
+wget https://sealyun-market.oss-accelerate.aliyuncs.com/marketctl/v1.0.2/linux_amd64/marketctl
 chmod a+x marketctl
 cat > marketctl_$1.yaml << EOF
 market:
