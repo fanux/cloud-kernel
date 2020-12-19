41478adac8a21de142ab4c2eedb8e3c5b9dd67bd
cuisongliu <cuisongliu@qq.com>
2020-12-19 23:23:21 +0800

    fix(master): fix ubuntu20的问题
    (cherry picked from commit f1b24f9bb8cda585f133a7cc41c3c62c3b68c039)

--- kube/shell/init.sh
+++ kube/shell/init.sh
@@ -66,9 +66,9 @@ cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.conf.all.rp_filter=0
+net.ipv4.ip_forward=1
 EOF
 sysctl --system
-sysctl -w net.ipv4.ip_forward=1
 disable_firewalld
 swapoff -a || true
 disable_selinux
